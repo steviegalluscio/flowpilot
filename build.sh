bb6c7e4673a9745ee78be0b18323832ad2a0e7ab
Fredrik Gustafsson <jagheterfredrik@gmail.com>
2024-11-09 00:50:46 +0100

    less user stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -t 1 ]
 then
-    docker run --rm -it -v $PWD:/work -v $PWD/cache/buildozer:/home/builder/.buildozer -v $PWD/cache/gradle:/home/builder/.gradle -w /work builder bash scripts/build.sh $@
+    docker run --rm -it -v $PWD:/work -v $PWD/cache/buildozer:/root/.buildozer -v $PWD/cache/gradle:/root/.gradle -w /work builder bash scripts/build.sh $@
 else
-    docker run --rm -v $PWD:/work -v $PWD/cache/buildozer:/home/builder/.buildozer -v $PWD/cache/gradle:/home/builder/.gradle -w /work builder bash scripts/build.sh $@
+    docker run --rm -v $PWD:/work -v $PWD/cache/buildozer:/root/.buildozer -v $PWD/cache/gradle:/root/.gradle -w /work builder bash scripts/build.sh $@
 fi
--- scripts/build.sh
+++ scripts/build.sh
@@ -41,7 +41,7 @@ build_scons() {
 # Function to build the app
 build_app() {
   echo "Building the app..."
-  ANDROID_HOME=/home/builder/.buildozer/android/platform/android-sdk ./gradlew assembleRelease
+  ANDROID_HOME="$HOME/.buildozer/android/platform/android-sdk" ./gradlew assembleRelease
 }
 
 # Function to build everything
