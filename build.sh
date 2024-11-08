4ebd06ccb0c271e143f0bc3c5ba8463fbe52a924
Fredrik Gustafsson <jagheterfredrik@gmail.com>
2024-11-08 21:12:27 +0100

    build tweaks

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -t 1 ]
 then
-    docker run --rm -it -u `id -u`:`id -g` -v ./:/work -v ./cache/buildozer:/home/builder/.buildozer -v ./cache/gradle:/home/builder/.gradle -w /work builder bash scripts/build.sh $@
+    docker run --rm -it -u `id -u`:`id -g` -v $PWD:/work -v $PWD/cache/buildozer:/home/builder/.buildozer -v $PWD/cache/gradle:/home/builder/.gradle -w /work builder bash scripts/build.sh $@
 else
-    docker run --rm -u `id -u`:`id -g` -v ./:/work -v ./cache/buildozer:/home/builder/.buildozer -v ./cache/gradle:/home/builder/.gradle -w /work builder bash scripts/build.sh $@
+    docker run --rm -u `id -u`:`id -g` -v $PWD:/work -v $PWD/cache/buildozer:/home/builder/.buildozer -v $PWD/cache/gradle:/home/builder/.gradle -w /work builder bash scripts/build.sh $@
 fi
