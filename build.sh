4fd0100ccaa65b2986e4a053aaa0c778bf8bb14d
Fredrik Gustafsson <jagheterfredrik@gmail.com>
2024-11-08 16:39:37 +0100

    allow for non-tty builds

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run --rm -it -u `id -u`:`id -g` -v ./:/work -v ./cache/buildozer:/home/builder/.buildozer -v ./cache/gradle:/home/builder/.gradle -w /work builder bash scripts/build.sh $@
+if [ -t 1 ]
+then
+    docker run --rm -it -u `id -u`:`id -g` -v ./:/work -v ./cache/buildozer:/home/builder/.buildozer -v ./cache/gradle:/home/builder/.gradle -w /work builder bash scripts/build.sh $@
+else
+    docker run --rm -u `id -u`:`id -g` -v ./:/work -v ./cache/buildozer:/home/builder/.buildozer -v ./cache/gradle:/home/builder/.gradle -w /work builder bash scripts/build.sh $@
+fi
