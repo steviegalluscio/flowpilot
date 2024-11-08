b7098d6ffd077d0852995cdf2d73657e24dfdbc3
Fredrik Gustafsson <jagheterfredrik@gmail.com>
2024-11-08 16:17:18 +0100

    match docker uid/guid to host

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it -v ./:/work -v ./cache/buildozer:/home/builder/.buildozer -v ./cache/gradle:/home/builder/.gradle -w /work builder ./scripts/build.sh $@
+docker run --rm -it -u `id -u`:`id -g` -v ./:/work -v ./cache/buildozer:/home/builder/.buildozer -v ./cache/gradle:/home/builder/.gradle -w /work builder bash scripts/build.sh $@
--- build_env.sh
+++ build_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -m 777 -p cache/buildozer cache/gradle
-docker build -t builder docker/
+docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -t builder docker/
